cdbba20984ed71b8b830e4704535672493a2be90
Rafal Kupczyk <grosik939393@gmail.com>
2022-12-19 12:42:19 +0100

    Implement sign out

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +15,21 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Jestes zalogowany jako: ${user.email} '),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Jestes zalogowany jako: ${user.email} '),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              child: Text('Wyloguj sie'),
+            ),
+          ],
+        ),
       ),
     );
   }
