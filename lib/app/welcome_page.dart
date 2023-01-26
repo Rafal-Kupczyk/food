8b1b623e9ba601560f5ed6074dd80f0e7c2b52f8
Rafal Kupczyk <grosik939393@gmail.com>
2023-01-26 20:04:47 +0100

    Some fixes

---
+++ lib/app/welcome_page.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:food/App/app.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/firstpage.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => const RootPage(),
+                  ),
+                );
+              },
+              child: const Text('Zaczynamy'),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
